91b299dd64f26915d7e0f858f1bb0b21161349db
Andikacipung <91275286+Andikacipung@users.noreply.github.com>
2021-09-23 22:47:08 +0700

    Add files via upload

---
+++ tugaske1.php
@@ -0,0 +1,32 @@
+<DOCTYPE html>
+<html>
+	<head>
+	<title> Data diri </title>
+	<style>
+
+	</style>
+	</head>
+		<body align= "center">
+		
+		<p> <img src="andika.jpg" align="center" width="100">
+		</p>
+		
+		
+		<?php
+		$nama = "Andika Pangestu"; 
+		$nim = 192520048;
+		$alamat = "Bagelen";
+		$alamat_email = "andikapangestucipung@gmail.com";
+		$bidang_minat = "Jaringan";
+		$universitas = "Universitas Muhammadiyah Purworejo";
+		?>
+					
+      	<p>nama: <?php echo $nama;?></p>
+        <p>nim: <?php echo $nim;?></p>
+	    <p>alamat: <?php echo $alamat;?></p></li>
+        <p>alamat_email: <?php echo $alamat_email;?></p>
+	    <p>bidang_minat: <?php echo $bidang_minat;?></p>
+        <p>universitas: <?php echo $universitas;?></p>
+	
+		</body>	
+</html>
